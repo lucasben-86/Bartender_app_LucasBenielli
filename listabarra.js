e521e5e982c9d3762213971c75fee3c504f98f7f
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-05-02 17:16:19 -0300

    preEntrega3 Benielli

--- listabarra.js
+++ listabarra.js
@@ -6,6 +6,6 @@ let listado = document.querySelector("#tragosGuardados");
 
 for (const trago of tragoLista){
   const li = document.createElement("ul");
-  li.innerText = trago.nombre;
+  li.innerHTML= `<h4><u>${trago.nombre}</u></h4><a href="${trago.recetaweb}" target="_blank" class="btn btn-outline-warning btn-sm">Ver Receta</a>`;
   listado.appendChild(li);
 }
