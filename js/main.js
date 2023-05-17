ee9ffbf0756f3138a2546ab9de9d051645dc0155
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-05-17 15:29:17 -0300

    agregado punteros

--- js/main.js
+++ js/main.js
@@ -7,12 +7,18 @@ const tragos = [
   {id: "whisky-busqueda", botella: "Whisky"}
 ];
 
-const listaTragos = document.querySelector("#lista-tragos");
 const listaTitulo = document.querySelector("#lista-titulo");
+const listaTragos = document.querySelector("#lista-tragos");
 
 tragos.forEach(trago => {
   const elementoTrago = document.querySelector(`#${trago.id}`);
   if (elementoTrago) {
+    elementoTrago.addEventListener("mouseover", () => {
+      elementoTrago.style.cursor = "pointer";
+    });
+    elementoTrago.addEventListener("mouseout", () => {
+      elementoTrago.style.cursor = "auto";
+    });
     elementoTrago.addEventListener("click", () => {
       window.location.href = "landing-page2.html?botella=" + trago.botella;
     });
@@ -29,13 +35,13 @@ window.addEventListener("DOMContentLoaded", async () => {
 
     const tragosFiltrados = data.filter(trago => trago.botella === botellaElegida);
     tragosFiltrados.forEach((trago, index) => {
-      listaTitulo.innerHTML = `<h3>Tragos con base alcohólica de ${botellaElegida}</h3>`;
+      listaTitulo.innerHTML = `<h5 class="mt-3">Tragos con base alcohólica de ${botellaElegida}</h5>`;
       const li = document.createElement("li");
       li.innerHTML = `<div class="card" style="width: 18rem;">
         <img src="${trago.imagen}" class="card-img-top" alt="...">
         <div class="card-body">
-          <p class="card-text">${trago.nombre}</p> 
-          <button type="button" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#receta-modal-${index}">Ver receta</button>
+          <p class="card-text text-center">${trago.nombre}</p> 
+          <button type="button" class="btn btn-outline-warning text-center" data-bs-toggle="modal" data-bs-target="#receta-modal-${index}">Ver receta</button>
           <div class="modal fade" id="receta-modal-${index}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog">
               <div class="modal-content">
@@ -47,7 +53,7 @@ window.addEventListener("DOMContentLoaded", async () => {
                 <h4 class="mb-3 text-center fw-bold text-warning">${trago.nombre}</h4>
                 <p class="text-end"><em>${trago.comentario}</em></p>
                 <p class="fw-bold text-warning"><u>Ingredientes:</u></p>
-                <ul>${trago.ingredientes.map((cadaPaso) => `<li>${cadaPaso}</li>`).join('')}</ul>
+                <ul>${trago.ingredientes.map((cadaPaso) => `<li>${cadaPaso}</li>`).join("")}</ul>
                 <p class="mt-4 fw-bold text-warning"><u>Preparación:</u></p>
                 <p class="text-justify">${trago.preparacion}</p>
               </div>
