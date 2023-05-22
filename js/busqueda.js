e1c18fd489c8141d57d1c161b9319e969005387f
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-05-22 14:26:07 -0300

    agrego: amplio json ver

--- js/busqueda.js
+++ js/busqueda.js
@@ -2,20 +2,28 @@ const textoBusqueda = document.querySelector("#buscar");
 const botonBusqueda = document.querySelector("#btnBuscar");
 const tituloBusqueda = document.querySelector("#titulo-busqueda");
 const listaBusqueda = document.querySelector("#lista-busqueda");
+const tragoBuscado = document.querySelector("#trago-buscado");
 
-botonBusqueda.addEventListener("click", () => {
-  const nombreTrago = textoBusqueda.value;
-  window.location.href = `lp-busqueda.html?nombre=${nombreTrago}`;
-});
-
-
-window.addEventListener("DOMContentLoaded", async () => {
-  const urlBusqueda = new URLSearchParams(window.location.search);
-  const tragoBuscado = urlBusqueda.get("nombre");
-
+botonBusqueda.addEventListener("click", async () => {
+  const valorBusqueda = textoBusqueda.value.toLowerCase();
+  
   const resp = await fetch("/js/tragos.json");
   const data = await resp.json();
+  
+  const resultado = data.filter(objeto => objeto.nombre.toLowerCase() === valorBusqueda);
 
-  console.log(tragoBuscado);
-  console.log(data);
+  if (resultado.length > 0) {
+    const nombreElegido = resultado[0].nombre;
+    listaBusqueda.textContent = nombreElegido;
+    const ul = document.createElement("ul");
+        ul.innerHTML = `<div class="card text-center mt-4" style="width: 18rem;">
+          <img src="${trago.imagen}" class="card-img-top img-fluid" alt="...">
+          <div class="card-body">
+            <h5 class="card-text text-center">${trago.nombre}</h5> 
+          </div>
+        </div>`;
+      tragoBuscado.appendChild(ul);
+  } else {
+    listaBusqueda.textContent = "No hay resultados";
+  }
 });
