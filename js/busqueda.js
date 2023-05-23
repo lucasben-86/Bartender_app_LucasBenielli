905c7e1cf453422c01c134d142ef450a0ac23abc
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-05-23 09:34:03 -0300

    agrego: emprolijo ok

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,8 +1,8 @@
 const textoBusqueda = document.querySelector("#buscar");
 const botonBusqueda = document.querySelector("#btnBuscar");
-const tituloBusqueda = document.querySelector("#titulo-busqueda");
 const listaBusqueda = document.querySelector("#lista-busqueda");
 const tragoBuscado = document.querySelector("#trago-buscado");
+const botonAleatorio = document.querySelector("#btnAleatorio");
 
 botonBusqueda.addEventListener("click", async () => {
   const valorBusqueda = textoBusqueda.value.toLowerCase();
@@ -45,6 +45,49 @@ botonBusqueda.addEventListener("click", async () => {
       tragoBuscado.appendChild(ul);
   } else {
     tragoBuscado.innerHTML = ""
-    listaBusqueda.textContent = "No hay resultados";
+    listaBusqueda.innerHTML = `<div class= "mt-4 text-warning">No hay resultados</div>`;
   }
 });
+
+botonAleatorio.addEventListener("click", async () => {
+  const resp = await fetch("/js/tragos.json");
+  const data = await resp.json();
+
+  function randomEntero(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+    }
+  
+    const aleatorioIndex = randomEntero(0, data.length - 1);
+    const tragoAleatorio = data[aleatorioIndex];
+    tragoBuscado.innerHTML = "";
+    listaBusqueda.innerHTML = "";
+    const ul = document.createElement("ul");
+        ul.innerHTML = `<div class="card text-center mt-4" style="width: 18rem;">
+          <img src="${tragoAleatorio.imagen}" class="card-img-top img-fluid" alt="...">
+          <div class="card-body">
+            <h5 class="card-text text-center">${tragoAleatorio.nombre}</h5> 
+            <button type="button" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#receta-modal">Ver receta</button>
+            <div class="modal fade" id="receta-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title fs-5" id="staticBackdropLabel2">Receta</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    <h4 class="mb-3 fw-bold text-warning">${tragoAleatorio.nombre}</h4>
+                    <p class="text-end"><em>${tragoAleatorio.comentario}</em></p>
+                    <p class="text-start fw-bold text-warning"><u>Ingredientes:</u></p>
+                    <ul class="text-start">${tragoAleatorio.ingredientes.map((cadaPaso) => `<li>${cadaPaso}</li>`).join("")}</ul>
+                    <p class="text-start mt-4 fw-bold text-warning"><u>Preparación:</u></p>
+                    <p class="text-justify">${tragoAleatorio.preparacion}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>`;
+      tragoBuscado.appendChild(ul);
+    })
--- js/test.js
+++
@@ -1,5 +0,0 @@
-// fetch("/js/tragos.json")
-// .then(res => res.json())
-// .then(data => console.log(data))
-
-// //con este fetch capturo el array limpio del json
